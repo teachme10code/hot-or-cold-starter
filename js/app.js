3db78dc8b2fca41a176f7a87c982a60f2d399b40
Chris <thinkfulchris@gmail.com>
2014-09-10 20:19:48 -0700

    Added initial functions per requirements

--- js/app.js
+++ js/app.js
@@ -11,7 +11,24 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-
 });
+
+//When the '+ New Game' link is clicked, the page is reloaded
+function newGame() {
+	location.reload();
+}
+
+//Generate a random number between 1 and 100
+function generateRandomNumber() {
+	var randomNumber = Math.floor(Math.random() * 100) + 1;
+	console.log(randomNumber);
+	return randomNumber;
+}
+
+//Evaluate the user's guess and return feedback
+function evaluateGuess() {
+	var userInputValue = $('#userGuess').val();
+	console.log(userInputValue);
+}
 
 
